1c24d065606ea570afec48e5327ac0708ee7a5ec
root <root@cloud.(none)>
2016-08-15 10:29:10 +0800

    new file 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>汗味周黑鸭</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="author" content="Wisdom">
+<meta name="keywords" content="鸭,黑鸭">
+<meta name="description" content="做鸭的网站">
+</head>
+<body>
+<?php
+	echo "dida:我做鸭，做黑鸭。";
+?>
+</body>
