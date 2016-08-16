8bb9aeb4f37cfce981ea562616cd13caf0c1a471
xiezhi <madder0809@163.com>
2016-08-16 16:23:33 +0800

    nothing

--- Application/Home/Controller/ShowController.class.php
+++ Application/Home/Controller/ShowController.class.php
@@ -2,7 +2,22 @@
 namespace Home\Controller;
 use Think\Controller;
 class ShowController extends Controller {
-    public function index(){
+    public function intruduce(){
+        $this->display();
+    }
+    public function cultural(){
+        $this->display();
+    }
+    public function team(){
+        $this->display();
+    }
+    public function news(){
+        $this->display();
+    }
+    public function dynamics(){
+        $this->display();
+    }
+    public function contact(){
         $this->display();
     }
 }
