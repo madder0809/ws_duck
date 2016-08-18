0f64f8b960f6d802aabdbfee68d82719ba4794e7
madder0809 <ytmftdkj1010@163.com>
2016-08-18 22:11:01 +0800

    update

---
+++ Application/Admin/Controller/ArticleController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Admin\Controller;
+
+class IndexController extends AdminController {
+	public function _initialize(){
+		parent::_initialize();
+		$type = I("get.type") ? I("get.type") : 1;//默认为1
+		$this->type = $type;
+	}
+
+    public function index(){
+    	$type = $this->type;
+    	$menu = array(1=>"新闻动态",2=>"行业动态",3=>"常见问题");
+        $this->display();
+    }
+}
