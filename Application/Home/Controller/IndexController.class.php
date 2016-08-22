6c43c677c1ae7374354c821607673d36f3ad0c24
madder0809 <ytmftdkj1010@163.com>
2016-08-22 23:28:44 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,6 +5,7 @@ class IndexController extends Controller {
     public function index(){
     	$data['news'] = M("article")->limit(5)->order("id DESC")->select();
     	$data['products'] = M("products")->limit(10)->order("id DESC")->select();
+    	$data['about'] = M("web_info")->where("type = 1")->find();
     	$this->assign($data);
         $this->display();
     }
