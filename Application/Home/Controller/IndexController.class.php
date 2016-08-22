4bd217becefa66e558db0d59a1fe6cf706f26908
xiezhi <madder0809@163.com>
2016-08-22 10:32:02 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$data['news'] = M("article")->limit(5)->order("id DESC")->select();
+    	$data['products'] = M("products")->limit(10)->order("id DESC")->select();
+    	$this->assign($data);
         $this->display();
     }
 }
